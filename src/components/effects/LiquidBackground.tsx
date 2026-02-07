86f7c678d20fc69291b209f9ff454bd9ac1d28af
Think Clear Hq <kingsrijanofficial@gmail.com>
2026-02-07 12:47:32 +0530

    Add files via upload

--- src/components/effects/LiquidBackground.tsx
+++ src/components/effects/LiquidBackground.tsx
@@ -37,7 +37,7 @@ export function LiquidBackground() {
 
             {/* Ambient Background Glow - Emerald/Teal */}
             <motion.div
-                className="absolute w-[1200px] h-[1200px] bg-emerald-900/10 rounded-full blur-[150px] opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                className="absolute w-[300px] h-[300px] md:w-[1200px] md:h-[1200px] bg-emerald-900/10 rounded-full blur-[60px] md:blur-[150px] opacity-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 will-change-transform transform-gpu"
                 animate={{
                     scale: [1, 1.1, 1],
                     opacity: [0.2, 0.3, 0.2],
@@ -47,7 +47,7 @@ export function LiquidBackground() {
 
             {/* REACTIVE CURSOR WAVE: Electric Cyan/Blue - Higher Opacity + Repel */}
             <motion.div
-                className="absolute w-[600px] h-[600px] bg-cyan-500/20 rounded-full blur-[100px] opacity-40 mix-blend-screen left-1/2 top-1/2"
+                className="hidden md:block absolute w-[600px] h-[600px] bg-cyan-500/20 rounded-full blur-[100px] opacity-40 mix-blend-screen left-1/2 top-1/2 will-change-transform"
                 style={{
                     x: initialX,
                     y: initialY,
@@ -56,7 +56,7 @@ export function LiquidBackground() {
 
             {/* REACTIVE CURSOR CORE: Bright White/Mint Highlight - Strongest Parallax + Opacity */}
             <motion.div
-                className="absolute w-[200px] h-[200px] bg-white/10 rounded-full blur-[50px] opacity-60 mix-blend-overlay left-1/2 top-1/2"
+                className="hidden md:block absolute w-[200px] h-[200px] bg-white/10 rounded-full blur-[50px] opacity-60 mix-blend-overlay left-1/2 top-1/2 will-change-transform"
                 style={{
                     x: coreX,
                     y: coreY,
@@ -65,7 +65,7 @@ export function LiquidBackground() {
 
             {/* Constant Background Waves - "Always Going On" */}
             <motion.div
-                className="absolute w-[800px] h-[800px] bg-emerald-600/20 rounded-full blur-[120px] opacity-25 top-[-20%] left-[-10%] mix-blend-screen"
+                className="absolute w-[400px] h-[400px] md:w-[800px] md:h-[800px] bg-emerald-600/20 rounded-full blur-[50px] md:blur-[120px] opacity-25 top-[-20%] left-[-10%] mix-blend-screen will-change-transform transform-gpu"
                 animate={{
                     x: [0, 80, -20, 0],
                     y: [0, 50, -30, 0],
@@ -75,7 +75,7 @@ export function LiquidBackground() {
             />
 
             <motion.div
-                className="absolute w-[700px] h-[700px] bg-teal-600/20 rounded-full blur-[100px] opacity-25 bottom-[-10%] right-[-10%] mix-blend-screen"
+                className="absolute w-[350px] h-[350px] md:w-[700px] md:h-[700px] bg-teal-600/20 rounded-full blur-[50px] md:blur-[100px] opacity-25 bottom-[-10%] right-[-10%] mix-blend-screen will-change-transform transform-gpu"
                 animate={{
                     x: [0, -60, 20, 0],
                     y: [0, -80, 40, 0],
@@ -86,7 +86,7 @@ export function LiquidBackground() {
 
             {/* Deep Violet/Indigo Contrast Wave - Slow & Deep */}
             <motion.div
-                className="absolute w-[1000px] h-[600px] bg-indigo-800/20 rounded-[100%] blur-[130px] opacity-20 bottom-[-30%] left-[20%] mix-blend-color-dodge"
+                className="absolute w-[400px] h-[300px] md:w-[1000px] md:h-[600px] bg-indigo-800/20 rounded-[100%] blur-[60px] md:blur-[130px] opacity-20 bottom-[-30%] left-[20%] mix-blend-color-dodge will-change-transform transform-gpu"
                 animate={{
                     x: [0, 50, -50, 0],
                     y: [0, -30, 30, 0],
